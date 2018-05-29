0256382c5ce8da42c1258f7542e79dd480ac309d
Jason Lattimer <jason.lattimer@gmail.com>
2018-05-28 21:51:35 -0500

    Added exception if AI key not populated

--- D365AppInsights.Shared.Tests.Common/Configs.cs
+++ D365AppInsights.Shared.Tests.Common/Configs.cs
@@ -1,14 +1,23 @@
-﻿namespace AppInsightsShared.Tests
+﻿using JLattimer.D365AppInsights;
+using System;
+
+namespace AppInsightsShared.Tests
 {
     public class Configs
     {
         public static AiSetup GetAiSetup(bool disableDependencyTracking, bool disableEventTracking,
-            bool disableExceptionTracking, bool disableMetricTracking, bool disableTraceTracking, 
+            bool disableExceptionTracking, bool disableMetricTracking, bool disableTraceTracking,
             bool disableContextParameterTracking, bool enableDebug)
         {
+            const string aiInstrumentationKey = "YOUR AI INSTRUMENTATION KEY";
+
+            if (aiInstrumentationKey == "YOUR AI INSTRUMENTATION KEY")
+                throw new Exception(
+                    "Enter your Application Insights instrumention key in D365AppInsights.Shared.Tests.Common.Configs");
+
             return new AiSetup
             {
-                InstrumentationKey = "YOUR AI INSTRUMENTATION KEY",
+                InstrumentationKey = aiInstrumentationKey,
                 AiEndpoint = "https://dc.services.visualstudio.com/v2/track",
                 DisableDependencyTracking = disableDependencyTracking,
                 DisableEventTracking = disableEventTracking,
