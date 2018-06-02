48ec0c9f5fe71b6d02fe795649394c0a16899bfd
Jason Lattimer <jason.lattimer@gmail.com>
2018-06-01 23:23:31 -0500

    Updated version and description explaining the whole mess with prelease dependencies

--- D365AppInsights.Js.NuGet/Properties/VersionInfo.cs
+++ D365AppInsights.Js.NuGet/Properties/VersionInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 
 // This should be the same version as below
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.0.1-PreRelease")]
+[assembly: AssemblyInformationalVersion("1.0.2-PreRelease")]
 #else
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 #endif
