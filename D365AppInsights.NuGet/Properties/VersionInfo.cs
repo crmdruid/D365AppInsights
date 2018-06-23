154d69a637676722f9c7e606cbf666259569b04d
Jason Lattimer <jason.lattimer@gmail.com>
2018-06-23 15:43:09 -0500

    Updated version

--- D365AppInsights.NuGet/Properties/VersionInfo.cs
+++ D365AppInsights.NuGet/Properties/VersionInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 
 // This should be the same version as below
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.0.2-PreRelease")]
+[assembly: AssemblyInformationalVersion("1.0.3-PreRelease")]
 #else
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
 #endif
